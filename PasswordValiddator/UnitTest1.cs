f6661535ccf591537b4ceeaed6e32a71680001aa
shahadAlroomi <shahad.ro@icloud.com>
2025-11-19 13:28:21 +0100

    krav 2

--- PasswordValiddator/UnitTest1.cs
+++ PasswordValiddator/UnitTest1.cs
@@ -5,6 +5,7 @@ public class UnitTest1
     [Theory]
     [InlineData("shahad2022")]
     [InlineData("Elin1238")]
+    [InlineData("wwe83ufl")]
 
     public void GetPassword (string password)
     {
