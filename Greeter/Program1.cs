d15296ddd22be19d93d4c697e4a4f6df24d37b64
shahadAlroomi <shahad.ro@icloud.com>
2025-11-19 11:11:26 +0100

    first commit

---
+++ Greeter/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+
+public class PasswordValidator
+{ 
+    public static void Main()
+    {
+        
+    }
+
+public static bool Validate(string password)
+    {
+    
+        return true;
+        
+       
+    }    
+}
+
+
---
+++ PasswordValiddator/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace Password.Validdator;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("shahad2022")]
+    [InlineData("Elin123")]
+
+    public void GetPassword (string password)
+    {
+    
+
+        //Assert 
+
+        Assert.True(PasswordValidator.Validate(password));
+
+
+    }
+}
